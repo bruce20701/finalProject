153de27bd2fce361fc97a6820460a09e0b9fae45
bruce20701 <131954867+bruce20701@users.noreply.github.com>
2023-06-04 16:11:11 +0800

    fuction test2

--- model/message.py
+++ model/message.py
@@ -1,8 +1,11 @@
-import weatherQurey
+from weatherQurey import weatherQurey
 from weather import Weather
 from datetime import datetime
 
-weather1 = Weather(weatherQurey.qureyWeatherbyCity("臺北市"))
-weather2 = Weather(weatherQurey.qureyNearestTimeWeather(datetime.now().strftime("%H:%M")))
-weather1.printWeather()
-weather2.printWeather()
+#模擬在Line打的message
+testMessage = "查詢天氣「臺北市」"
+
+#判斷message為查詢天氣就呼叫weatherQurey function
+if(testMessage[:4] == "查詢天氣"):
+    weather =  Weather(weatherQurey(testMessage[5:8]))
+    weather.printWeather()
